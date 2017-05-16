4f0b5014c5bff738f074545af54f6bc58d62524b
Cristian Chies <cristian.chies@cwi.com.br>
2017-05-16 12:39:13 -0300

    testes envio de mensagem para aplicativos client

--- messaging/app/src/main/java/com/google/firebase/quickstart/fcm/MainActivity.java
+++ messaging/app/src/main/java/com/google/firebase/quickstart/fcm/MainActivity.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2016 Google Inc. All Rights Reserved.
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,6 +16,7 @@
 
 package com.google.firebase.quickstart.fcm;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -26,9 +27,21 @@ import android.widget.Toast;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.messaging.FirebaseMessaging;
 
+import org.json.JSONObject;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
+    private static final String LEGACY_SERVER_KEY = "AIzaSyD7q8WziubAnYdNY3vjChaim-6bS4iSz3c";
+    // esse registration token deve ser o token do app destinatario da mensagem, atualmente está pegando do aplicativo atual
+    // isso deve ser pego no aplicativo client e guardado nas informações ddo usuário do AVD manager
+    private String regToken;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,15 +84,54 @@ public class MainActivity extends AppCompatActivity {
         logTokenButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Get token
-                String token = FirebaseInstanceId.getInstance().getToken();
+                // Get regToken
+                regToken = FirebaseInstanceId.getInstance().getToken();
 
                 // Log and toast
-                String msg = getString(R.string.msg_token_fmt, token);
+                String msg = getString(R.string.msg_token_fmt, regToken);
                 Log.d(TAG, msg);
                 Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    public static final MediaType JSON
+            = MediaType.parse("application/json; charset=utf-8");
+
+    /**
+     * Metodo pego desse link:http://stackoverflow.com/questions/37435750/how-to-send-device-to-device-messages-using-firebase-cloud-messaging </br>
+     * Para enviar mensagens entre apps
+     * @param reg_token
+     */
+    private void sendNotification(final String reg_token) {
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... params) {
+                try {
+                    OkHttpClient client = new OkHttpClient();
+                    JSONObject json = new JSONObject();
+                    JSONObject dataJson = new JSONObject();
+                    dataJson.put("body", "Hi this is sent from device to device");
+                    dataJson.put("title", "dummy title");
+                    json.put("notification", dataJson);
+                    json.put("to",
+
+                            regToken );
+                    RequestBody body = RequestBody.create(JSON, json.toString());
+                    Request request = new Request.Builder()
+                            .header("Authorization", "key=" + LEGACY_SERVER_KEY)
+                            .url("https://fcm.googleapis.com/fcm/send")
+                            .post(body)
+                            .build();
+                    Response response = client.newCall(request).execute();
+                    String finalResponse = response.body().string();
+                } catch (Exception e) {
+                    Log.d(TAG, e + "");
+                }
+                return null;
+            }
+        }.execute();
+
     }
 
 }
